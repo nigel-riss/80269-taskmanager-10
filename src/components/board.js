2ae4039fe238bdfc2d7cfe81434a2cedb1ba7cee
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2019-12-10 01:00:50 +0200

    Move elements to seperate component files

---
+++ src/components/board.js
@@ -0,0 +1,20 @@
+/**
+ * Creates Board template
+ * @return {string} Board markup
+ */
+const createBoardTemplate = () => {
+  return `
+    <section class="board container">
+      <div class="board__filter-list">
+        <a href="#" class="board__filter">SORT BY DEFAULT</a>
+        <a href="#" class="board__filter">SORT BY DATE up</a>
+        <a href="#" class="board__filter">SORT BY DATE down</a>
+      </div>
+
+      <div class="board__tasks">
+      </div>
+    </section>
+  `;
+};
+
+export {createBoardTemplate};
