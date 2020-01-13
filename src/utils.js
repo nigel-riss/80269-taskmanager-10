191574c4ae53a5efb402f0f3e1ae3ccd78ee30ad
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-01-13 08:16:30 +0200

    Add new render function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,40 @@
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+};
+
+
+/**
+ * Returns HTMLElement from template string
+ * @param {String} template HTML markup
+ * @return {HTMLElement}
+ */
+export const createElement = (template) => {
+  const newElement = document.createElement(`div`);
+  newElement.innerHTML = template;
+
+  return newElement.firstChild;
+};
+
+
+/**
+ * Renders HTMLElement into another HTMLElement
+ * @param {HTMLElement} container
+ * @param {HTMLElement} element
+ * @param {String} place
+ */
+export const render = (container, element, place) => {
+  switch (place) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(element);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(element);
+      break;
+  }
+};
+
+
 /**
  * Returns a random integer in a range
  * @param {Number} min minimal number (included)
