06af57066e1ccb79393175d161becef2d35ba45b
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2020-01-20 00:53:41 +0200

    5.1. Refactor site-menu to inherit from abstract-component

--- src/components/site-menu.js
+++ src/components/site-menu.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils';
+import AbstractComponent from './abstract-component';
 
 /**
  * Creates Site Menu template
@@ -37,24 +37,8 @@ const createSiteMenuTemplate = () => {
   );
 };
 
-export default class SiteMenu {
-  constructor() {
-    this._element = null;
-  }
-
+export default class SiteMenu extends AbstractComponent {
   getTemplate() {
     return createSiteMenuTemplate();
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
